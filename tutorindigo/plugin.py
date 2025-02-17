0871a0c3560d647becc2adb40be5d6b75c6bf4a8
Muhammadaziz-dev <baxtiyorovmuhammadaziz4@gmail.com>
2025-02-17 21:24:45 +0500

    changes

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -28,13 +28,13 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
         "FOOTER_NAV_LINKS": [
-            {"title": "About Us", "url": "/about"},
+            {"title": "Biz haqimizda", "url": "/about"},
             {"title": "Blog", "url": "/blog"},
-            {"title": "Donate", "url": "/donate"},
-            {"title": "Terms of Service", "url": "/tos"},
-            {"title": "Privacy Policy", "url": "/privacy"},
-            {"title": "Help", "url": "/help"},
-            {"title": "Contact Us", "url": "/contact"},
+            {"title": "Xayriya qiling", "url": "/donate"},
+            {"title": "Xizmat shartlari", "url": "/tos"},
+            {"title": "Maxfiylik siyosati", "url": "/privacy"},
+            {"title": "Yordam", "url": "/help"},
+            {"title": "Biz bilan bog'lanish", "url": "/contact"},
         ],
     },
     "unique": {},
